4a06e673f36630a57d343043ca3ba7206f6cb58c
ameenmahmood <ameenbmahmood@gmail.com>
2026-03-07 23:21:14 -0500

    Add 'PK' to visited countries list

--- make-map.py
+++ make-map.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 def main():
 
     # List of countries you've visited (can also use ISO codes)
-    visited_countries = {'US', 'MX', 'BZ', 'HN', 'CA', 'VN', 'TH', 'SG', 'DE', 'QA'}
+    visited_countries = {'US', 'MX', 'BZ', 'HN', 'CA', 'VN', 'TH', 'SG', 'DE', 'QA', 'PK'}
     about_to_visit = {}
 
     # Load world geometries
